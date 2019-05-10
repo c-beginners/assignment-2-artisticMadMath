c60e5ad7ac9773619749214fee80cd1b9ff2fc33
public-float-artMajor <ryan.alex.davis@gmail.com>
2019-05-10 02:24:47 -0700

    Started looking for chars in input

--- CharCounter/CharCounter/CharCount.cpp
+++ CharCounter/CharCounter/CharCount.cpp
@@ -3,5 +3,41 @@
 
 using std::cout;
 using std::endl;
+using std::string;
 
 
+void CharCounter(char input[]) {
+   //int charCounter[];
+
+}
+
+string GetFoundChars(char input[]) {
+   // int size = 0;
+   string foundChars = "";
+
+   for (int i = 0; input[i] != '\0'; i++) {
+      for (int j = 0; j < foundChars.size()-1; j++) {
+      // if (input[i] not in foundChars)
+         if ( (foundChars[j] == '\0') || (input[i] != foundChars[j]) ) {
+            foundChars = foundChars + input[i];
+         }
+      }
+   }
+
+   return foundChars;
+}
+
+
+int main() {
+
+   return 0;
+}
+
+
+/* In a given array of chars, find out what character is included the most times.
+ E.g: ”A, C, C, B, B, A, C” and also print how many times it occours,
+ for this example it would be ”C: 3”. Try to do this in as low time-complexity
+ as possible. (This question will come in handy if you do intervjues for
+ developer jobs)
+ Bonus task, print the first occourance of a douplicated char
+ */
